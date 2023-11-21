4675f53e87826088b6976bc9ac55fcfb3195b265
tylersmith <tyjsmith1@gmail.com>
2023-11-21 15:55:46 -0500

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from "react"
+import { name, city } from "../data/data.js"
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const customStyle = {
+    color: "firebrick",
+  };
+
+  return (
+    <div id="home">
+      <h1 style={customStyle}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
